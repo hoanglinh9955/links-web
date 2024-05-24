36f9006e797351d195a3f14efbab45ff09f87bfe
LinhHoang <hoanglinh9955@gmail.com>
2024-05-24 14:31:16 +0700

    dev chi-tiet-page

---
+++ pages/chi-tiet-san-pham/[id].vue
@@ -0,0 +1,49 @@
+<script setup>
+const links = [{
+  label: 'Trang Chủ',
+  icon: 'i-heroicons-home',
+  to: '/'
+}, {
+  label: 'Sản Phẩm',
+  to: '/san-pham',
+  icon: 'i-heroicons-building-storefront'
+}, {
+  label: 'Chi Tiết Sản Phẩm',
+  icon: 'i-heroicons-shopping-bag',
+}]
+
+const items = [
+  'https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lgn67eznbx9u70',
+  'https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lgn67ezndbuad5',
+  'https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lgn67ezneqeqc5',
+  'https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lgn67eznhjjma2'
+]
+
+// const { data } = await useFetch(`https://linkss.pages.dev/api/products/getProduct`, {
+//       query: { product_id: 'pro1' }
+//     })
+//     if (data.value.success) {
+//       orderList.splice(0, orderList.length);
+//       orderList.push(data.value.results)
+//     }
+</script>
+
+<template>
+    <UContainer class="flex flex-col space-y-5"> 
+        <UBreadcrumb class="pt-3" :links="links" />
+        <UDivider size="xs" />
+        <div class="flex w-full space-x-5">
+            <div class="w-[500px] h-[500px]">
+                <UCarousel v-slot="{ item }" :items="items" :ui="{ item: 'basis-full' }" class="rounded-lg overflow-hidden" indicators>
+                    <img :src="item" class="w-full" draggable="false">
+                  </UCarousel>
+            </div>
+            <div class="w-1/2 flex flex-col space"> 
+                <h3 class="text-4xl">Nồi lẩu điện mini</h3>
+                <h4 class="text-5xl">69.000VND</h4>
+            </div>
+        </div>
+
+    </UContainer>
+  
+</template>
--- pages/san-pham.vue
+++ pages/san-pham.vue
@@ -1,6 +1,67 @@
 <template>
-  <div>
-    san-pham
+  <div class="flex flex-col space-y-28">
+    <!-- san pham ban chay div -->
+    <UContainer class="flex flex-col justify-center items-center pt-16">
+      <UDivider size="xs" :ui="{label :'text-4xl'}" label="Sản Phẩm Bán Chạy" />
+      <div class="flex flex-col justify-center items-center pt-10">
+        <div class="flex space-x-5">
+          <CardContainer />
+          <CardContainer />
+          <CardContainer />
+          <CardContainer />
+        </div>
+        <NuxtLink class="pt-5 w-full flex justify-end mr-8 items-center font-medium" to="/san-pham">
+          Xem Thêm  <Icon class="m-1" name="i-heroicons-chevron-double-right"/>
+        </NuxtLink>
+      </div>
+    </UContainer>
+    <!-- san pham ban chay div -->
+    <UContainer class="flex flex-col justify-center items-center">
+      <UDivider size="xs" :ui="{label :'text-4xl'}" label="Sản Phẩm Bán Chạy" />
+      <div class="flex flex-col justify-center items-center pt-10">
+        <div class="flex space-x-5">
+          <CardContainer />
+          <CardContainer />
+          <CardContainer />
+          <CardContainer />
+        </div>
+        <NuxtLink class="pt-5 w-full flex justify-end mr-8 items-center font-medium" to="/san-pham">
+          Xem Thêm  <Icon class="m-1" name="i-heroicons-chevron-double-right"/>
+        </NuxtLink>
+      </div>
+    </UContainer>
+
+    <!-- san pham ban chay div -->
+    <UContainer class="flex flex-col justify-center items-center">
+      <UDivider size="xs" :ui="{label :'text-4xl'}" label="Sản Phẩm Bán Chạy" />
+      <div class="flex flex-col justify-center items-center pt-10">
+        <div class="flex space-x-5">
+          <CardContainer />
+          <CardContainer />
+          <CardContainer />
+          <CardContainer />
+        </div>
+        <NuxtLink class="pt-5 w-full flex justify-end mr-8 items-center font-medium" to="/san-pham">
+          Xem Thêm  <Icon class="m-1" name="i-heroicons-chevron-double-right"/>
+        </NuxtLink>
+      </div>
+    </UContainer>
+
+    <!-- san pham ban chay div -->
+    <UContainer class="flex flex-col justify-center items-center pb-8">
+      <UDivider size="xs" :ui="{label :'text-4xl'}" label="Sản Phẩm Bán Chạy" />
+      <div class="flex flex-col justify-center items-center pt-10">
+        <div class="flex space-x-5">
+          <CardContainer />
+          <CardContainer />
+          <CardContainer />
+          <CardContainer />
+        </div>
+        <NuxtLink class="pt-5 w-full flex justify-end mr-8 items-center font-medium" to="/san-pham">
+          Xem Thêm  <Icon class="m-1" name="i-heroicons-chevron-double-right"/>
+        </NuxtLink>
+      </div>
+    </UContainer>
   </div>
 </template>
 
